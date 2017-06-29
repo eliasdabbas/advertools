796da0a230b052be3dbe82dda09733a8d5fec8b4
eliasdabbas <eliasdabbas@gmail.com>
2017-06-29 22:08:19 +0200

    Fix setup.py
    add the following
    - long_description
    - change version to 0.1a1 to indicate alpha
    - fix intended audience and topic
    - add license and programming language details

--- setup.py
+++ setup.py
@@ -4,18 +4,27 @@ from codecs import open
 from os import path
 
 setup(
-    name='Advertools',
-    version='0.1',
+    name='advertools',
+    version='0.1a1',
     description='Productivity and analysis tools for online marketing',
+    long_description='A set of tools that help online marketing people '
+                     'create campaigns, manage them, analyze keywords,'
+                     'and more. One of the main use cases is generating'
+                     'combinations of keywords for setting up large search'
+                     'campaigns, together with all the tasks that come with'
+                     'setting up campaigns, ad groups, and ads.',
+    url='https://github.com/eliasdabbas/advertools',
     author='Elias Dabbas',
     author_email='eliasdabbas@gmail.com',
     license='MIT',
     keywords='marketing advertising adwords analytics seo sem',
     install_requires=['pandas'],
-
     classifiers=[
-        'Intended Audience :: Online / Digital Marketers',
-        'Topic :: Online Marketing :: Digital Marketing'
-    ]
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Internet',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
 
 )
