54d29d42b34d834c6f8f6345b0acc264ea209ad5
eliasdabbas <eliasdabbas@gmail.com>
2017-07-09 04:26:41 +0400

    Add import separate_rows

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -5,6 +5,7 @@ from advertools.kw_combinations import *
 from advertools.kw_match_types import *
 from advertools.objects import *
 from advertools.permute import *
+from advertools.separate_rows import *
 from advertools.utm_builder import *
 from advertools.word_frequency import *
 
