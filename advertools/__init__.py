279c27788de8a472ae912cd7db6acb254ea4cd1a
eliasdabbas <eliasdabbas@gmail.com>
2017-07-15 17:41:52 +0400

    Add import for num_format

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -3,6 +3,7 @@ from advertools.categorical_distribution import *
 from advertools.expand import *
 from advertools.kw_combinations import *
 from advertools.kw_match_types import *
+from advertools.num_format import num_format
 from advertools.objects import *
 from advertools.permute import *
 from advertools.separate_rows import *
