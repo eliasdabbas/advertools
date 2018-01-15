aeaa02241e9e6eea8b7042d8c2b2f3a6a82e427a
eliasdabbas <eliasdabbas@gmail.com>
2018-01-15 06:23:48 +0400

    add url-encoding to utm_builder

--- advertools/utm_builder.py
+++ advertools/utm_builder.py
@@ -1,21 +1,32 @@
-from urllib.parse import urlparse, urljoin
+import urllib
 
 
-def utm_builder(url, source, medium=None, campaign_name=None,
-                content=None, term=None):
-    url = (url + '?') if '?' not in url else (url + '&')
-    source = 'utm_source=' + source
-    url += source
-    if medium:
-        medium = '&utm_medium=' + medium
-        url += medium
-    if campaign_name:
-        campaign_name = '&utm_campaign=' + campaign_name
-        url += campaign_name
-    if content:
-        content = '&utm_content=' + content
-        url += content
-    if term:
-        term = '&utm_term=' + term
-        url += term
-    return url
+def utm_builder(url, utm_source, utm_medium=None, utm_campaign=None,
+                utm_content=None, utm_term=None):
+    """Generate a utm-encoded URL for your campaigns. 
+
+    URLs are are not validated. All parameters are URL-encoded.
+
+    Parameters
+    ----------
+    url : a valid URL, required
+    utm_source : the referrer of the traffic (e.g. facebook, twitter), required
+    utm_medium : marketing medium (e.g. banner, email)
+    utm_campaign : the name of the campaign (e.g. summer_promo, 20pct_off)
+    utm_content : ad name / differentiator (e.g. 728x90, mpu, square_banner)
+    utm_term : search terms bid on (only relevant for search campaigns)
+
+    Examples
+    --------
+    >>> utm_builder('mysite.com', utm_source='the source)
+    ... 'mysite.com?utm_source=the+source'
+
+    >>> utm_builder('mysite.com', utm_source='the source', 
+        utm_medium='THE MEDIUM!!', utm_campaign='campaign*name&^%', 
+        utm_content='728x90')
+    ... 'mysite.com?utm_content=728x90&utm_campaign=campaign%2Aname%26%5E%25&utm_medium=THE+MEDIUM%21%21&utm_source=the+source'
+
+    """
+    url += '?'
+    params = {k: v for k, v in locals().items() if k != 'url'}
+    return url + urllib.parse.urlencode({k: v for k, v in params.items() if v})
