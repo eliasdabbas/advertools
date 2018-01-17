d0211ff3377166cb92e84573325b89b463b78232
eliasdabbas <eliasdabbas@gmail.com>
2018-01-17 06:48:06 +0400

    reformat docstring

--- advertools/kw_generate.py
+++ advertools/kw_generate.py
@@ -1,14 +1,20 @@
 
 from itertools import permutations
 
+import pandas as pd
+
 def kw_generate(products, words, max_len=3, match_types=['Exact', 'Phrase', 'Broad'],
                 campaign_name='SEM_Campaign'):
     """Generate a data frame of kewywords using a list of products and relevant words.
-    `products` will be used as the nams of the ad groups
-    `words` are any related words that make it clear that the user is interested in `products`
-    `max_len` is the maximum number of words to include in each permutation to product keywords
-    `match_types` can be restricted or kept as is based on preference
-    `campaign_name` name of campaign! 
+    
+    Parameters
+    ----------
+    
+    products : will be used as the names of the ad groups
+    words : related words that make it clear that the user is interested in `products`
+    max_len : the maximum number of words to include in each permutation of product keywords
+    match_types : can be restricted or kept as is based on preference
+    campaign_name : name of campaign
     
     >>> products = ['bmw', 'toyota']
     >>> words = ['buy', 'second hand']
@@ -20,6 +26,12 @@ def kw_generate(products, words, max_len=3, match_types=['Exact', 'Phrase', 'Bro
     2  SEM_Campaign      bmw          bmw buy          Broad
     3  SEM_Campaign      bmw  bmw second hand          Exact
     4  SEM_Campaign      bmw  bmw second hand         Phrase
+    
+    Returns
+    -------
+    
+    keywords_df : a pandas.DataFrame ready to upload
+    
     """
     headers = ['Campaign', 'Ad Group', 'Keyword', 'Criterion Type']
     keywords_list = []
