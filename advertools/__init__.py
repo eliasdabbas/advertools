f31e858d3e41a5dc0e751101223ba2c38a932eea
eliasdabbas <eliasdabbas@gmail.com>
2018-01-24 17:15:36 +0100

    Add import stopwords to __init__

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -1,4 +1,5 @@
 from advertools.ad_from_string import ad_from_string
 from advertools.kw_generate import kw_generate
+from advertools.stopwords import stopwords
 from advertools.url_builders import url_utm_ga
 from advertools.word_frequency import word_frequency
