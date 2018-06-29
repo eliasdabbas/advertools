61640ac72bb2b64bbbff4e41d861144d13e1fee8
eliasdabbas <eliasdabbas@gmail.com>
2018-06-29 22:27:32 +0200

    Make init import all kw_match_type functions

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -1,7 +1,7 @@
 """Productivity and analysis tools for online marketing"""
 from advertools.ad_create import ad_create
 from advertools.ad_from_string import ad_from_string
-from advertools.kw_generate import kw_generate
+from advertools.kw_generate import *
 from advertools.stopwords import stopwords
 from advertools.url_builders import url_utm_ga
 from advertools.word_frequency import word_frequency
