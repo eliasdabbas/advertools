a24959564b17638573f3bb2b1a8d35bccb81cbaa
eliasdabbas <eliasdabbas@gmail.com>
2018-07-06 21:18:16 +0400

    Modify init file to include author, email, version

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -1,8 +1,14 @@
-"""Productivity and analysis tools for online marketing"""
-from advertools.ad_create import ad_create
-from advertools.ad_from_string import ad_from_string
-from advertools.kw_generate import *
-from advertools.stopwords import stopwords
-from advertools.url_builders import url_utm_ga
-from advertools.word_frequency import word_frequency
-from .version import __version__
+# -*- coding: utf-8 -*-
+
+"""Top-level package for advertest."""
+
+__author__ = """Elias Dabbas"""
+__email__ = 'eliasdabbas@gmail.com'
+__version__ = '0.2.0'
+
+from advertest.ad_create import ad_create
+from advertest.ad_from_string import ad_from_string
+from advertest.kw_generate import *
+from advertest.stopwords import stopwords
+from advertest.url_builders import url_utm_ga
+from advertest.word_frequency import word_frequency
