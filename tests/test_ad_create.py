c8010c939d3dd292e746e503858e2626535a74c8
eliasdabbas <eliasdabbas@gmail.com>
2018-07-06 20:00:13 +0400

    Modify old tests mainly using pytest

--- tests/test_ad_create.py
+++ tests/test_ad_create.py
@@ -1,33 +1,32 @@
-import unittest
-import sys
-sys.path.insert(1, '/advertools/')
-from advertools.ad_create import ad_create
+from advertest.ad_create import ad_create
 
+import pytest
 
-class AdCreateTests(unittest.TestCase):
-    
-    def test_raises_error_for_long_input_strings(self):
-        with self.assertRaises(ValueError):
-            ad_create('short template {}', ['one', 'two', 'three'], 'very long fallback string', 20)
-        with self.assertRaises(ValueError):
-            ad_create('very long template string {}', ['one', 'two', 'three'], 'short', 20)
-            
-    def test_all_replacements_used(self):
-        replacements = ['one', 'two', 'three']
-        result = ad_create('Hello {}', replacements, 'fallback', capitalize=False)
-        self.assertTrue(all([rep in ' '.join(result) for rep in replacements]))
-    
-    def test_fallback_used_if_string_long(self):
-        replacements = ['one', 'two', 'three hundrend thousand']
-        result = ad_create('Hello {}', replacements, 'fallback', max_len=20, capitalize=False)
-        self.assertEqual(result, ['Hello one', 'Hello two', 'Hello fallback'])
+def test_raises_error_for_long_input_strings():
+    with pytest.raises(ValueError):
+        ad_create('short template {}', ['one', 'two', 'three'],
+                  'very long fallback string', 20)
+    with pytest.raises(ValueError):
+        ad_create('very long template string {}', ['one', 'two', 'three'],
+                  'short', 20)
 
-    def test_final_string_capitalized_or_not(self):
-        capitalized = ad_create('heLLo {}', ['ONE', 'tWo', 'tHree', 'Four'], 'fallback', capitalize=True)
-        not_capitalized = ad_create('heLLo {}', ['ONE', 'tWo', 'tHree', 'Four'], 'fallback', capitalize=False)
-        self.assertEqual(capitalized, ['Hello One', 'Hello Two', 'Hello Three', 'Hello Four'])
-        self.assertEqual(not_capitalized, ['heLLo ONE', 'heLLo tWo', 'heLLo tHree', 'heLLo Four'])
+def test_all_replacements_used():
+    replacements = ['one', 'two', 'three']
+    result = ad_create('Hello {}', replacements, 'fallback', capitalize=False)
+    assert all([rep in ' '.join(result) for rep in replacements])
 
+def test_fallback_used_if_string_long():
+    replacements = ['one', 'two', 'three hundrend thousand']
+    result = ad_create('Hello {}', replacements, 'fallback', max_len=20,
+                       capitalize=False)
+    assert result == ['Hello one', 'Hello two', 'Hello fallback']
 
-if __name__ == '__main__':
-        unittest.main()
+def test_final_string_capitalized_or_not():
+    capitalized = ad_create('heLLo {}', ['ONE', 'tWo', 'tHree', 'Four'],
+                            'fallback', capitalize=True)
+    not_capitalized = ad_create('heLLo {}', ['ONE', 'tWo', 'tHree', 'Four'],
+                                'fallback', capitalize=False)
+    assert capitalized == ['Hello One', 'Hello Two', 'Hello Three',
+                           'Hello Four']
+    assert not_capitalized == ['heLLo ONE', 'heLLo tWo', 'heLLo tHree',
+                               'heLLo Four']
--- tests/test_kw_generate.py
+++ tests/test_kw_generate.py
@@ -1,20 +1,17 @@
 import unittest
 from itertools import permutations, combinations
 
-import os, sys
-sys.path.insert(0, '..')
-
-from advertools.kw_generate import kw_generate
+from advertest.kw_generate import *
 
 import pandas as pd
-
+import pytest
 
 class KeywordTests(unittest.TestCase):
     
     def test_match_types_are_capitalized(self):
         df = kw_generate(['one', 'two'], ['three', 'four'], match_types=['eXact', 'PHRase', 'BRoad'])
         self.assertEqual(set(df['Criterion Type']), {'Exact', 'Phrase', 'Broad'})
-        
+
     def test_raises_error_for_wrong_match_types(self):
         with self.assertRaises(ValueError):
             kw_generate(['one', 'two'], ['three', 'four'], match_types=['hello', 'world'])
@@ -22,30 +19,30 @@ class KeywordTests(unittest.TestCase):
     def test_raises_error_for_wrong_max_len(self):
         with self.assertRaises(ValueError):
             kw_generate(['one', 'two'], ['three', 'four'], max_len=1)
-            
+
     def test_right_combinatorics_fuction_selected(self):
         df_perm = kw_generate(['one', 'two'], ['three', 'four'], order_matters=True)
-        self.assertTrue({'one three', 'three one'}.issubset(set(df_perm['Keyword']))) 
+        self.assertTrue({'one three', 'three one'}.issubset(set(df_perm['Keyword'])))
         df_comb = kw_generate(['one', 'two'], ['three', 'four'], order_matters=False)
-        self.assertFalse({'one three', 'three one'}.issubset(set(df_comb['Keyword']))) 
-        
+        self.assertFalse({'one three', 'three one'}.issubset(set(df_comb['Keyword'])))
+
     def test_correct_column_names_used(self):
         headers = ['Campaign', 'Ad Group', 'Keyword', 'Criterion Type', 'Labels']
         df = kw_generate(['one', 'two'], ['three', 'four'])
         self.assertEqual(list(df.columns.values), headers)
-        
+
     def test_all_products_in_final_result(self):
         products = {'One', 'Two', 'Three'}
         df = kw_generate(products, ['four', 'five'])
         self.assertEqual(products, set(df['Ad Group']))
-        
+
     def test_keyword_lengths_vary_between_2_max_len(self):
         df2 = kw_generate(['one', 'two'], ['three', 'four'], max_len=2)
         self.assertEqual(set(df2['Keyword'].str.count(' ')+1), {2})
         df3 = kw_generate(['one', 'two'], ['three', 'four'], max_len=3)
         self.assertEqual(set(df3['Keyword'].str.count(' ')+1), {2, 3})
-        df4 = kw_generate(['one', 'two'], ['three', 'four', 'five'], max_len=4)        
-        self.assertEqual(set(df4['Keyword'].str.count(' ')+1), {2, 3, 4})        
+        df4 = kw_generate(['one', 'two'], ['three', 'four', 'five'], max_len=4)
+        self.assertEqual(set(df4['Keyword'].str.count(' ')+1), {2, 3, 4})
 
     def test_correct_combinations_are_generated(self):
         products = ['one', 'two']
@@ -58,7 +55,7 @@ class KeywordTests(unittest.TestCase):
                         continue
                     test_combs.append(' '.join(comb))
 
-        df = kw_generate(products, words, match_types=['Exact'], order_matters=False)        
+        df = kw_generate(products, words, match_types=['Exact'], order_matters=False)
         self.assertEqual(set(test_combs), set(df['Keyword']))
 
     def test_correct_permutations_are_generated(self):
@@ -72,9 +69,9 @@ class KeywordTests(unittest.TestCase):
                         continue
                     test_combs.append(' '.join(perm))
 
-        df = kw_generate(products, words, match_types=['Exact'], order_matters=True)        
+        df = kw_generate(products, words, match_types=['Exact'], order_matters=True)
         self.assertEqual(set(test_combs), set(df['Keyword']))
-        
+
     def test_correct_campaign_name(self):
         df = kw_generate(['one', 'two'], ['three'], campaign_name='My_campaign')
         self.assertEqual(df['Campaign'][0], 'My_campaign')
@@ -88,7 +85,7 @@ class KeywordTests(unittest.TestCase):
         df = kw_generate(['one', 'two'], ['three'], match_types=['Modified'])
         self.assertTrue(all(df['Keyword'].str.count('\+') == df['Keyword'].str.count(' ')+1))
         self.assertTrue(set(df['Criterion Type']) == {'Broad'})
-    
+
     def test_all_match_types_used(self):
         df_exact = kw_generate(['one'], ['two'], match_types=['Exact'])
         df_phrase = kw_generate(['one'], ['two'], match_types=['Phrase'])
@@ -97,12 +94,55 @@ class KeywordTests(unittest.TestCase):
         self.assertEqual(set(df_exact['Criterion Type']), {'Exact'})
         self.assertEqual(set(df_phrase['Criterion Type']), {'Phrase'})
         self.assertEqual(set(df_broad['Criterion Type']), {'Broad'})
-        self.assertEqual(set(df_e_p_b['Criterion Type']), {'Exact', 'Phrase', 'Broad'})        
+        self.assertEqual(set(df_e_p_b['Criterion Type']), {'Exact', 'Phrase', 'Broad'})
 
     def test_returns_pd_dataframe(self):
         df = kw_generate(['one'], ['two'], order_matters=True)
         self.assertEqual(type(df), pd.core.frame.DataFrame)
         self.assertEqual(df.shape, (6, 5))
+
+words = ['[one]', '"two"', '+three -four', '[five', 'six"',
+         '+seven eight nine]']
+
+
+def test_kw_broad():
+    result = kw_broad(words)
+    assert result == ['one', 'two', 'three -four', 'five', 'six',
+                      'seven eight nine']
+
+def test_kw_exact():
+    result = kw_exact(words)
+    assert result == ['[one]', '[two]', '[three -four]', '[five]', '[six]',
+                      '[seven eight nine]']
+
+def test_exact_contains_brackets():
+    result = kw_exact(words)
+    assert ['[' in x and ']' in x for x in result]
+
+def test_kw_phrase():
+    result = kw_phrase(words)
+    assert result == ['"one"', '"two"', '"three -four"', '"five"', '"six"',
+                      '"seven eight nine"']
+
+def test_kw_modified():
+    result = kw_modified(words)
+    assert result == ['+one', '+two', '+three +-four', '+five', '+six',
+                      '+seven +eight +nine']
+
+def test_kw_neg_broad():
+    result = kw_neg_broad(words)
+    assert result == ['-one', '-two', '-three -four', '-five', '-six',
+                      '-seven eight nine']
+
+def test_kw_neg_exact():
+    result = kw_neg_exact(words)
+    assert result == ['-[one]', '-[two]', '-[three -four]', '-[five]', '-[six]',
+                      '-[seven eight nine]']
+
+def test_kw_neg_phrase():
+    result = kw_neg_phrase(words)
+    assert result == ['-"one"', '-"two"', '-"three -four"', '-"five"', '-"six"',
+                      '-"seven eight nine"']
 
 if __name__ == '__main__':
         unittest.main()
