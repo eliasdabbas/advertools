f879a374057e9fefebe934657c5ba0e9490eb214
eliasdabbas <eliasdabbas@gmail.com>
2018-07-06 20:05:55 +0400

    Modify setup py

--- setup.py
+++ setup.py
@@ -1,32 +1,59 @@
+#!/usr/bin/env python
+
+"""The setup script."""
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    'pandas'
+]
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+]
 
 setup(
-    name='advertools',
-    version='0.1.1',
-    description='Productivity and analysis tools for online marketing',
-    long_description='A set of tools that help online marketing people '
-                     'create campaigns, manage them, analyze keywords,'
-                     'and more. One of the main use cases is generating'
-                     'combinations of keywords for setting up large search'
-                     'campaigns, together with all the tasks that come with'
-                     'setting up campaigns, ad groups, and ads.',
-    url='https://github.com/eliasdabbas/advertools',
-    author='Elias Dabbas',
+    author="Elias Dabbas",
     author_email='eliasdabbas@gmail.com',
-    license='MIT',
-    keywords='marketing advertising adwords analytics seo sem',
-    install_requires=['pandas'],
-    python_requires='>=3',
-    packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Internet',
+        'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-
+    description="Productivity and analysis tools for online marketing",
+#    entry_points={
+#        'console_scripts': [
+#            'advertest=advertest.cli:main',
+#        ],
+#    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='advertising marketing search-engine-optimization adwords '
+             'seo sem bingads keyword-research',
+    name='advertools',
+    packages=find_packages(include=['advertools']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/eliasdabbas/advertools',
+    version='0.2.0',
+    zip_safe=False,
 )
