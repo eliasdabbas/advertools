8cf0f2a58e97d893d6ef21589cc6e999ab1b589c
eliasdabbas <eliasdabbas@gmail.com>
2018-07-10 18:10:02 +0400

    Make extract_ module import explicit not *

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -8,7 +8,7 @@ __version__ = '0.2.0'
 
 from advertools.ad_create import ad_create
 from advertools.ad_from_string import ad_from_string
-from advertools.extract import *
+from advertools.extract import extract_mentions, extract_hashtags
 from advertools.kw_generate import *
 from advertools.stopwords import stopwords
 from advertools.url_builders import url_utm_ga
