6381aaf5817d5ea07913f172ce8657af22fca1ff
eliasdabbas <eliasdabbas@gmail.com>
2018-07-11 02:37:35 +0400

    Docs modifications to word_frequency and ad_from_string

--- advertools/ad_from_string.py
+++ advertools/ad_from_string.py
@@ -1,25 +1,27 @@
 import string
+
+
 def ad_from_string(s, slots=(30, 30, 80, 15, 15), sep=None, capitalize=False):
-    """Convert string `s` to an ad by splitting it into groups of words.
-    Each group would have a length of at most the allowed length for that slot. 
-    
-    If the total length of `s` exceeds the total allowed length, all remaining
+    """Convert string ``s`` to an ad by splitting it into groups of words.
+    Each group would have a length of at most the allowed length for that slot.
+
+    If the total length of ``s`` exceeds the total allowed length, all remaining
     characters would be grouped in the last element of the returned list.
-        
+
     :param s: a string of characters, with no restrictions on length.
-    :param slots: an iterable of integers for the maximum lengths for each slot
-    :param sep: by which character to split `s`
-    :param capitalize: whether or not to capitalize each word after grouping. Setting
-        it as False would leave the input string as is. 
-    :returns text_ad: a list of strings
+    :param slots: an iterable of integers for the maximum lengths for each slot.
+    :param sep: by which character to split ``s``.
+    :param capitalize: whether or not to capitalize each word after grouping.
+        Setting it as False would leave the input string as is.
+    :returns text_ad: a list of strings.
 
     >>> ad_from_string('this is a short ad')
-    ... ['This Is A Short Ad', '', '', '', '', '']
-    
-    >>> ad_from_string('this is a longer ad and would take two of the first slots')
-    ... ['This Is A Longer Ad And Would', 'Take Two Of The First Slots', '', '', '', '']
+    ['This Is A Short Ad', '', '', '', '', '']
+
+    >>> ad_from_string('this is a longer ad and would take the first two slots')
+    ['This Is A Longer Ad And Would', 'Take The First Two Slots', '', '', '', '']
     """
-    
+
     str_words = s.split(sep=sep)
     text_ad = ['' for x in range(len(slots)+1)]
     counter = 0
@@ -30,7 +32,8 @@ def ad_from_string(s, slots=(30, 30, 80, 15, 15), sep=None, capitalize=False):
                 break
             text_ad[i] += ' ' + str_words[counter] if text_ad[i] else str_words[counter]
             counter += 1
-                  
-    text_ad[-1] = sep.join(str_words[counter:]) if sep is not None else ' '.join(str_words[counter:]) 
+
+    text_ad[-1] = (sep.join(str_words[counter:])
+                   if sep is not None else ' '.join(str_words[counter:]))
 
     return [string.capwords(x) if capitalize else x for x in text_ad]
--- advertools/word_frequency.py
+++ advertools/word_frequency.py
@@ -3,67 +3,74 @@ from collections import defaultdict
 import advertools as adv
 import pandas as pd
 
-def word_frequency(text_list, num_list, sep=None,
-                   rm_words=adv.stopwords['english']):
-    """Count the absolute as well as the weighted frequency of words
-    in `text_list` (based on `num_list`).
 
-    :param text_list: terable of strings.
+def word_frequency(text_list, num_list, sep=None,
+                   rm_words=adv.stopwords['english'], extra_info=False):
+    """Count the absolute as well as the weighted frequency of words
+    in ``text_list`` (based on ``num_list``).
+
+    :param text_list: iterable of strings.
         Typically short phrases, but could be any list of full blown documents.
         Usually, you would use this to analyze tweets, book titles, URLs, etc.
     :param num_list: iterable of numbers.
-        A list of numbers with the same length as `text_list`, describing a
+        A list of numbers with the same length as ``text_list``, describing a
         certain attribute of these 'documents'; views, retweets, sales, etc.
     :param sep: string.
         The seperator with which to separate words in each document.
         The default uses white spaces, and you can specify another separator.
     :param rm_words: iterable of strings.
         Words to remove from the list 'stop-words'. The default uses
-        `nltk`'s list of English stopwords. To get all available languages
-        run `adv.stopwords.keys()`
+        ``nltk``'s list of English stopwords. To get all available languages
+        run ``adv.stopwords.keys()``
+    :param extra_info: boolean.
+        Whethr or not to give additional columns about the frequencies
     :returns abs_wtd_df: absolute and weighted DataFrame.
-        pandas.DataFrame with several metrics calculated. The most important are
-        `abs_freq` and `wtd_freq`. These show the difference between the number
-        of occurrences of each word together with their respective weighted
-        occurrences (frequency vs. weighted frequency). Other metrics are also
-        provided. The columns are as follows:
+        pandas.DataFrame with several metrics calculated. The most important
+        are ``abs_freq`` and ``wtd_freq``. These show the difference between the
+        number of occurrences of each word together with their respective
+        weighted occurrences (frequency vs. weighted frequency). Other metrics
+        are also provided. The columns are as follows:
 
-        word : Word.
+        word: Word.
             Words in the document list each on its own row.
-        abs_freq : Absolute frequency.
+        abs_freq: Absolute frequency.
             The number of occurrences of each word in all the documents.
-        abs_perc : Absolute frequency percentage.
-            `abs_freq` divided by the sum of all occurrences of words.
-        abs_perc_cum : Cumulative absolute percentage.
-            Cumulative sum of `abs_perc` to see how many words form x% of the occurrences.
-        wtd_freq : Weighted frequency.
-            Every occurrence of `word` multiplied by its respective value in
-            `num_list` provided by the function call.
-        wtd_freq_perc : `wtd_freq` divided by the total weighted frequencies of
+        wtd_freq: Weighted frequency.
+            Every occurrence of ``word`` multiplied by its respective value in
+            ``num_list`` provided by the function call.
+        rel_value: Relative value.
+            ``wtd_freq`` divided by ``abs_freq``, showing the value per occurrence
+            of ``word``
+
+        :extra_info:
+
+        abs_perc: Absolute frequency percentage.
+            ``abs_freq`` divided by the sum of all occurrences of words.
+        abs_perc_cum: Cumulative absolute percentage.
+            Cumulative sum of ``abs_perc`` to see how many words form x% of
+            the occurrences.
+        wtd_freq_perc: ``wtd_freq`` divided by the total weighted frequencies of
             all words.
-        wtd_freq_perc_cum : Cumulative weighted frequncy percentage.
-            Cumulative sum of `wtd_freq_perc`to see how many words form x% of
+        wtd_freq_perc_cum: Cumulative weighted frequncy percentage.
+            Cumulative sum of ``wtd_freq_perc`` to see how many words form x% of
             the weighted occurrences.
-        rel_value : Relative value.
-            `wtd_freq` divided by `abs_freq`, showing the value per occurrence
-            of `word`
 
     >>> text_list = ['apple orange', 'apple orange banana',
                      'apple kiwi', 'kiwi mango']
     >>> num_list = [100, 100, 100, 400]
 
-    >>> adv.word_frequency(text_list, num_list)[['word', 'abs_freq', 'wtd_freq']]
-         word  abs_freq  wtd_freq
-    0    kiwi         2       500
-    1   mango         1       400
-    2   apple         3       300
-    3  orange         2       200
-    4  banana         1       100
+    >>> adv.word_frequency(text_list, num_list)
+         word  abs_freq  wtd_freq  rel_value
+    0    kiwi         2       500      250.0
+    1   mango         1       400      400.0
+    2   apple         3       300      100.0
+    3  orange         2       200      100.0
+    4  banana         1       100      100.0
 
     "kiwi" occurred twice (abs_freq), one of these phrases has a score of 100,
     and the other 400, so the wtd_freq is the sum (100 + 400 = 500)
 
-    >>> adv.word_frequency(text_list, num_list)
+    >>> adv.word_frequency(text_list, num_list, extra_info=True)
          word  abs_freq  abs_perc  abs_perc_cum  wtd_freq  wtd_freq_perc  wtd_freq_perc_cum  rel_value
     0    kiwi         2  0.222222      0.222222       500       0.333333           0.333333      250.0
     1   mango         1  0.111111      0.333333       400       0.266667           0.600000      400.0
@@ -86,15 +93,18 @@ def word_frequency(text_list, num_list, sep=None,
     columns = {0: 'abs_freq', 1: 'wtd_freq'}
 
     abs_wtd_df = (pd.DataFrame.from_dict(word_freq, orient='index')
-                  .rename(columns=columns )
+                  .rename(columns=columns)
                   .sort_values('wtd_freq', ascending=False)
                   .assign(rel_value=lambda df: df['wtd_freq'] / df['abs_freq'])
                   .round())
-
-    abs_wtd_df.insert(1, 'abs_perc', value=abs_wtd_df['abs_freq'] / abs_wtd_df['abs_freq'].sum())
-    abs_wtd_df.insert(2, 'abs_perc_cum', abs_wtd_df['abs_perc'].cumsum())
-    abs_wtd_df.insert(4, 'wtd_freq_perc', abs_wtd_df['wtd_freq'] / abs_wtd_df['wtd_freq'].sum())
-    abs_wtd_df.insert(5, 'wtd_freq_perc_cum', abs_wtd_df['wtd_freq_perc'].cumsum())
+    if extra_info:
+        abs_wtd_df.insert(1, 'abs_perc', value=abs_wtd_df['abs_freq'] /
+                          abs_wtd_df['abs_freq'].sum())
+        abs_wtd_df.insert(2, 'abs_perc_cum', abs_wtd_df['abs_perc'].cumsum())
+        abs_wtd_df.insert(4, 'wtd_freq_perc', abs_wtd_df['wtd_freq'] /
+                          abs_wtd_df['wtd_freq'].sum())
+        abs_wtd_df.insert(5, 'wtd_freq_perc_cum',
+                          abs_wtd_df['wtd_freq_perc'].cumsum())
 
     abs_wtd_df = abs_wtd_df.reset_index().rename(columns={'index': 'word'})
 
