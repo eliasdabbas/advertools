41dbc098826807431ca7385ad86fe13d97263060
eliasdabbas <eliasdabbas@gmail.com>
2018-07-11 19:05:41 +0400

    Reformat tests

--- tests/test_ad_create.py
+++ tests/test_ad_create.py
@@ -2,6 +2,7 @@ from advertools.ad_create import ad_create
 
 import pytest
 
+
 def test_raises_error_for_long_input_strings():
     with pytest.raises(ValueError):
         ad_create('short template {}', ['one', 'two', 'three'],
@@ -10,16 +11,19 @@ def test_raises_error_for_long_input_strings():
         ad_create('very long template string {}', ['one', 'two', 'three'],
                   'short', 20)
 
+
 def test_all_replacements_used():
     replacements = ['one', 'two', 'three']
     result = ad_create('Hello {}', replacements, 'fallback', capitalize=False)
     assert all([rep in ' '.join(result) for rep in replacements])
+
 
 def test_fallback_used_if_string_long():
     replacements = ['one', 'two', 'three hundrend thousand']
     result = ad_create('Hello {}', replacements, 'fallback', max_len=20,
                        capitalize=False)
     assert result == ['Hello one', 'Hello two', 'Hello fallback']
+
 
 def test_final_string_capitalized_or_not():
     capitalized = ad_create('heLLo {}', ['ONE', 'tWo', 'tHree', 'Four'],
--- tests/test_ad_from_string.py
+++ tests/test_ad_from_string.py
@@ -1,27 +1,32 @@
 from advertools.ad_from_string import ad_from_string
 
-import pytest
 
 def test_len_result_one_more_than_len_slots():
     for i in range(5):
         result = ad_from_string('sample text', slots=[10 for i in range(i)])
         assert len(result) == i + 1
 
+
 def test_result_same_as_input_text():
     input_text = 'This is sample text to input'
     result = ad_from_string(input_text)
     assert ' '.join(result).strip() == input_text
 
+
 def test_long_string_gets_skipped():
     result = ad_from_string('This_is_looooooooooooooooooooong')
     assert result[0] == ''
 
+
 def test_capitalizes_or_not():
     s = 'This text will be split by the Function'
     capitalized = ad_from_string(s, capitalize=True)
-    assert capitalized == ['This Text Will Be Split By The', 'Function', '', '', '', '']
+    assert capitalized == ['This Text Will Be Split By The',
+                           'Function', '', '', '', '']
     not_capitalized = ad_from_string(s, capitalize=False)
-    assert not_capitalized == ['This text will be split by the', 'Function', '', '', '', '']
+    assert not_capitalized == ['This text will be split by the',
+                               'Function', '', '', '', '']
+
 
 def test_result_lengths_within_slots():
     s = 'some random text that will be split by different slot lengths'
