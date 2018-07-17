76c1a757a65b3cc944cb9f669b1d8ebe6b8bb7c4
eliasdabbas <eliasdabbas@gmail.com>
2018-07-17 14:04:31 +0200

    Create test for supplying only stopwords + linting

--- tests/test_word_frequency.py
+++ tests/test_word_frequency.py
@@ -1,8 +1,5 @@
 from advertools.word_frequency import word_frequency
-import advertools as adv
-import pandas as pd
 
-import pytest
 
 sep_list = [None, ' ', '-', '_']
 
@@ -17,7 +14,7 @@ text_list = [
 ]
 
 num_list = [
-    100, 
+    100,
     200,
     300,
     400,
@@ -33,30 +30,40 @@ separators = [
     'f'
 ]
 
+
 def test_len_result_one_more_than_len_slots():
     for sep in sep_list:
         result = word_frequency(text_list, num_list, sep=sep)
         if sep is not None:
             assert sep not in result['word']
 
+
 def test_rm_words_removed():
     result = word_frequency(text_list, num_list, rm_words=['one', 'two'])
     assert 'one' not in result['word']
     assert 'two' not in result['word']
 
+
 def test_extra_info_not_provided():
     result = word_frequency(text_list, num_list, extra_info=False)
-    assert set(result.columns.values) ==  {'word', 'abs_freq', 'wtd_freq', 'rel_value'}
+    assert set(result.columns.values) == {'word', 'abs_freq', 'wtd_freq',
+                                          'rel_value'}
+
 
 def test_extra_info_provided():
     result = word_frequency(text_list, num_list, extra_info=True)
-    assert set(result.columns.values) ==  {'word', 'abs_freq', 'abs_perc',
-                                           'abs_perc_cum', 'wtd_freq',
-                                           'wtd_freq_perc', 'wtd_freq_perc_cum',
-                                           'rel_value'}
+    assert set(result.columns.values) == {'word', 'abs_freq', 'abs_perc',
+                                          'abs_perc_cum', 'wtd_freq',
+                                          'wtd_freq_perc', 'wtd_freq_perc_cum',
+                                          'rel_value'}
 
 
 def test_words_separated_with_given_sep():
     for sep in separators:
         result = word_frequency(text_list, num_list, sep=sep)
         assert sep not in result['word']
+
+
+def test_works_fine_with_only_stopwords():
+    result = word_frequency(['on'], [3])
+    assert result.shape == (0, 4)
