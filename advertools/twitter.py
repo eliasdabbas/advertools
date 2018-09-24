a13cba657c67c3a9f2ab0004f0ae51d01ac50d15
eliasdabbas <eliasdabbas@gmail.com>
2018-09-24 18:40:32 +0200

    Add tweets_to_dataframe function

--- advertools/twitter.py
+++ advertools/twitter.py
@@ -20,3 +20,46 @@ def authenticate(func):
     wrapper.set_auth_params = set_auth_params
     wrapper.get_auth_params = get_auth_params
     return wrapper
+
+def tweets_to_dataframe(func):
+    @wraps(func)
+    def wrapper(count=None, trim_user=False, *args, **kwargs):
+        nonlocal func
+        twtr = Twython(**wrapper.get_auth_params()) 
+
+        func = eval('twtr.' + func.__name__)
+        count = count or 15
+        pages = (count // 100) + 1
+
+        tweets_responses = []
+        for i in range(pages):
+            try:
+                tweets = func(max_id=None if i == 0 else
+                                  tweets_responses[-1]['statuses'][-1]['id'] - 1,
+                              count=min([100, count]),
+                              *args, **kwargs)
+                tweets_responses.append(tweets)
+            except TwythonRateLimitError as e:
+                break
+        tweets_users_df = pd.DataFrame()
+        for tweets in tweets_responses:
+            if func.__name__ == 'search':
+                tweets_temp = pd.DataFrame(tweets['statuses'])
+            else:
+                tweets_temp = pd.DataFrame(tweets)
+            tweets_temp.columns = ['tweet_' + t for t in tweets_temp.columns]
+            if func.__name__ == 'search':
+                users_temp = pd.DataFrame([x['user'] for x in tweets['statuses']])
+            else:
+                users_temp = pd.DataFrame([x['user'] for x in tweets])
+            users_temp.columns = ['user_' + u for u in users_temp.columns]
+            tweets_users_temp = pd.concat([tweets_temp,users_temp], axis=1)
+            tweets_users_df = tweets_users_df.append(tweets_users_temp,
+                                                     ignore_index=True, sort=False)
+
+        tweets_users_df['tweet_source'] = tweets_users_df['tweet_source'].str.extract('nofollow">(.*)</a>')[0]
+        tweets_users_df['tweet_created_at'] = pd.to_datetime(tweets_users_df['tweet_created_at'])
+        tweets_users_df['user_created_at'] = pd.to_datetime(tweets_users_df['user_created_at'])
+
+        return tweets_users_df
+    return wrapper
