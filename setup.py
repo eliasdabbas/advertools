a67f3f645854a2bf6eabc39280a9b86bb2b2b9d3
eliasdabbas <eliasdabbas@gmail.com>
2018-11-04 03:25:07 +0100

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eliasdabbas/advertools',
-    version='0.4.1',
+    version='0.5.0',
     zip_safe=False,
 )
