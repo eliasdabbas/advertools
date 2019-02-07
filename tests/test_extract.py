7d2db9f29f3316ee97ccb540f5aa7be7774fef93
eliasdabbas <eliasdabbas@gmail.com>
2019-02-07 04:24:09 +0100

    Create tests for extract_words

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,6 @@
 
-from advertools.extract import extract_mentions, extract_hashtags, extract_emoji
+from advertools.extract import (extract_mentions, extract_hashtags,
+                                extract_emoji, extract_words)
 
 mention_posts = ['hello @name', 'email@domain.com', '@oneword',
                  'hi @nam-e and @name', '@first @last', 'an @under_score',
@@ -25,6 +26,16 @@ emoji_summary = extract_emoji(emoji_posts)
 emoji_test_keys = ['emoji', 'emoji_text', 'emoji_flat', 'emoji_flat_text',
                    'emoji_counts', 'emoji_freq', 'top_emoji', 'top_emoji_text', 
                    'overview']
+
+word_posts = ['today it is raining', 'i like rain and raining',
+              'today it is snowing', 'now snowing and raining',
+              'training is good with snow', 'RaIn and SNoW',
+              'none of the words', '@rain, #snow rain']
+
+word_summary_full = extract_words(word_posts, ['rain', 'snow'], True)
+word_summary_not_full = extract_words(word_posts, ['rain', 'snow'], False)
+word_test_keys = ['words', 'words_flat', 'word_counts',
+                  'word_freq', 'top_words', 'overview']
 
 
 def test_mention_result_has_correct_keys():
@@ -86,7 +97,6 @@ def test_correct_hashtags_extracted():
                                            ['#123text'], ['#_before', '#after_']]
 
 
-
 def test_correct_flat_hashtags():
     assert hashtag_summary['hashtags_flat'] == ['#name', '#oneword', '#nam',
                                                 '#name', '#first', '#last',
@@ -111,7 +121,6 @@ def test_correct_top_hashtags():
                                                ('#one', 1), ('#two', 1),
                                                ('#123text', 1), ('#مرحبا', 1),
                                                ('#_before', 1), ('#after_', 1)])
-
 
 
 def test_correct_hashtag_overview():
@@ -152,3 +161,68 @@ def test_correct_emoji_overview():
     assert emoji_overview['num_emoji'] == 3
     assert emoji_overview['emoji_per_post'] == 1.0
     assert emoji_overview['unique_emoji'] == 2
+
+
+
+def test_word_result_has_correct_keys():
+    assert set(word_summary_full.keys()) == set(word_test_keys)
+    assert set(word_summary_not_full.keys()) == set(word_test_keys)
+
+
+def test_correct_words_extracted():
+    assert word_summary_full['words'] == [[], ['rain'], [], [], ['snow'],
+                                          ['rain', 'snow'], [], ['rain']]
+    assert word_summary_not_full['words'] == [['raining'], ['rain', 'raining'],
+                                              ['snowing'],
+                                              ['snowing', 'raining'],
+                                              ['training', 'snow'],
+                                              ['rain', 'snow'], [],
+                                              ['@rain,', '#snow', 'rain']]
+
+
+def test_correct_flat_words():
+    assert word_summary_full['words_flat'] == ['rain', 'snow', 'rain', 'snow',
+                                               'rain']
+    assert word_summary_not_full['words_flat'] == ['raining', 'rain', 'raining',
+                                                   'snowing', 'snowing',
+                                                   'raining', 'training',
+                                                   'snow', 'rain', 'snow',
+                                                   '@rain,', '#snow', 'rain']
+
+
+def test_correct_word_counts():
+    assert word_summary_full['word_counts'] == [0, 1, 0, 0, 1, 2, 0, 1]
+    assert word_summary_not_full['word_counts'] == [1, 2, 1, 2, 2, 2, 0, 3]
+
+
+def test_correct_word_freq():
+    assert word_summary_full['word_freq'] == [(0, 4), (1, 3), (2, 1)]
+    assert word_summary_not_full['word_freq'] == [(0, 1), (1, 2),
+                                                  (2, 4), (3, 1)]
+
+
+def test_correct_top_words():
+    assert set(word_summary_full['top_words']) == {('rain', 3), ('snow', 2)}
+    assert set(word_summary_not_full['top_words']) == {('rain', 3), ('raining', 3),
+                                                       ('snow', 2), ('snowing', 2),
+                                                       ('#snow', 1), ('@rain,', 1),
+                                                       ('training', 1)}
+
+
+def test_correct_word_overview():
+    word_overview_full = word_summary_full['overview']
+    assert word_overview_full['num_posts'] == 8
+    assert word_overview_full['num_words'] == 5
+    assert word_overview_full['words_per_post'] == 5/8
+    assert word_overview_full['unique_words'] == 2
+
+    word_overview_not_full = word_summary_not_full['overview']
+    assert word_overview_not_full['num_posts'] == 8
+    assert word_overview_not_full['num_words'] == 13
+    assert word_overview_not_full['words_per_post'] == 13 / 8
+    assert word_overview_not_full['unique_words'] == 7
+
+
+def test_extract_words_puts_str_in_list():
+    word_summary_str = extract_words(word_posts, 'rain',  True)
+    assert word_summary_str['top_words'][0][0] == 'rain'
