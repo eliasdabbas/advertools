6bff70ec731bde338356bde7204a1ba67cc9b7b6
eliasdabbas <eliasdabbas@gmail.com>
2019-04-02 18:41:07 +0200

    Add test to check word_tokenize converts str to list

--- tests/test_word_tokenize.py
+++ tests/test_word_tokenize.py
@@ -6,3 +6,9 @@ def test_word_tokenize_splits_by_correct_number():
     for i in range(1, 4):
         result = word_tokenize(s, i)
         assert {word.count(' ') for word in result[0]}.pop() == i - 1
+
+
+def test_word_tokenize_converts_str_to_list():
+    s = 'this is a normal string'
+    result = word_tokenize(s)
+    assert isinstance(result, list)
