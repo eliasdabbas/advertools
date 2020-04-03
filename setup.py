c032204d9d1531693f1dde6e5f395cff826b31ec
eliasdabbas <eliasdabbas@gmail.com>
2020-04-04 01:38:43 +0200

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eliasdabbas/advertools',
-    version='0.8.1',
+    version='0.9.0',
     zip_safe=False,
 )
