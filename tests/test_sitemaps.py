4c4e2c57dccfc862e6db470ad699681abc4ad1d9
eliasdabbas <eliasdabbas@gmail.com>
2020-06-03 23:36:28 +0200

    Create tests for sitemap (that are actually used)

--- tests/test_sitemaps.py
+++ tests/test_sitemaps.py
@@ -1,34 +1,64 @@
-from advertools.sitemaps import sitemap_to_df, robotstxt_to_df
+import os
+from advertools.sitemaps import sitemap_to_df
 import pandas as pd
 import pytest
 
-tests_dir = 'https://raw.githubusercontent.com/eliasdabbas/advertools/master/tests/'
 
-sitemap_url = tests_dir + 'sitemap.xml'
-sitemap_gz_url = tests_dir + 'sitemap.xml.gz'
-sitemap_index_url = tests_dir + 'sitemap-index.xml'
+def full_path(file):
+    tests_dir = 'tests/sitemap_testing/'
+    return 'file://' + os.path.abspath(tests_dir + file)
 
-
-# def test_regular_sitemap():
-#     result = sitemap_to_df(sitemap_url)
-#     assert isinstance(result, pd.core.frame.DataFrame)
-#     assert len(result) == 5
-
-
-# def test_gz_sitemap():
-#     result = sitemap_to_df(sitemap_gz_url)
-#     assert isinstance(result, pd.core.frame.DataFrame)
-#     assert len(result) == 5
+regular_sitemap_url = full_path('regular_sitemap.xml')
+zipped_sitemap_url = full_path('zipped_sitemap.xml.gz')
+sitemap_index_url = full_path('sitemap_index.xml')
+error_sitemap_url = full_path('error_sitemap.xml')
+image_sitemap_url = full_path('image_sitemap.xml')
+video_sitemap_url = full_path('video_sitemap.xml')
+news_sitemap_url = full_path('news_sitemap.xml')
+robotstxt_url = full_path('robots.txt')
 
 
-# def test_sitemap_index():
-#     result = sitemap_to_df(sitemap_index_url)
-#     assert isinstance(result, pd.core.frame.DataFrame)
-#     assert len(result) == 6
+def test_regular_sitemap():
+    result = sitemap_to_df(regular_sitemap_url)
+    assert isinstance(result, pd.core.frame.DataFrame)
+    assert len(result) == 5
 
 
-# def test_robotstxt():
-#     result = robotstxt_to_df('https://www.google.com/robots.txt')
-#     assert isinstance(result, pd.core.frame.DataFrame)
-#     assert result.columns == ['directive', 'content', 'robotstxt_url',
-#                               'file_downloaded']
+def test_gz_sitemap():
+    result = sitemap_to_df(zipped_sitemap_url)
+    assert isinstance(result, pd.core.frame.DataFrame)
+    assert len(result) == 5
+
+
+def test_sitemap_index():
+    result = sitemap_to_df(sitemap_index_url)
+    assert isinstance(result, pd.core.frame.DataFrame)
+    assert all([col in result
+               for col in ['loc', 'sitemap_downloaded', 'sitemap']])
+
+
+def test_error_sitemap():
+    with pytest.raises(Exception):
+        result = sitemap_to_df(error_sitemap_url)
+
+
+def test_image_sitemap():
+    result = sitemap_to_df(image_sitemap_url)
+    assert isinstance(result, pd.core.frame.DataFrame)
+    assert 'image' in result
+
+
+def test_video_sitemap():
+    result = sitemap_to_df(video_sitemap_url)
+    assert isinstance(result, pd.core.frame.DataFrame)
+    assert 'video_content_loc' in result
+
+
+def test_news_sitemap():
+    result = sitemap_to_df(news_sitemap_url)
+    assert isinstance(result, pd.core.frame.DataFrame)
+    assert 'news' in result
+
+
+def test_get_sitemaps_from_robotstxt():
+    result = sitemap_to_df(robotstxt_url)
