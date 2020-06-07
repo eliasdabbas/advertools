392776fdd7f8709af173373afaf1a0ccebcdbac4
eliasdabbas <eliasdabbas@gmail.com>
2020-06-07 23:54:46 +0200

    Release 0.10.4

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Elias Dabbas"""
 __email__ = 'eliasdabbas@gmail.com'
-__version__ = '0.10.3'
+__version__ = '0.10.4'
 
 from advertools.ad_create import ad_create
 from advertools.ad_from_string import ad_from_string
