f739dab546e43b9f6ad602dc037136d3c3be8ec9
Elias Dabbas <eliasdabbas@gmail.com>
2021-01-30 19:40:35 +0400

    Add tests for crawllogs_to_df

---
+++ tests/test_logs.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+from advertools.logs import crawllogs_to_df
+
+
+def full_local_path(folder, file):
+    tests_dir = f'tests/data/{folder}/'
+    return os.path.abspath(tests_dir + file)
+
+
+def test_crwallogs_returns_df():
+    for logfile in os.listdir('tests/data/logs_testing'):
+        result = crawllogs_to_df(full_local_path('logs_testing', logfile))
+        assert isinstance(result, pd.core.frame.DataFrame)
