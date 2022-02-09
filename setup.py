d13b6e7be5ba3f22e33672c1ac563cac74bcd035
Elias Dabbas <eliasdabbas@gmail.com>
2022-02-09 15:17:50 +0100

    Bump version to v0.13.0.rc2

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eliasdabbas/advertools',
-    version='0.13.0.rc1',
+    version='0.13.0.rc2',
     zip_safe=False,
 )
