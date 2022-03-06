107bf2865fb980312f3b6abc4f7d927f03ded67e
Elias Dabbas <eliasdabbas@gmail.com>
2022-03-06 19:17:03 +0100

    Bump version to v0.14.0a6

--- advertools/__init__.py
+++ advertools/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Elias Dabbas"""
 __email__ = 'eliasdabbas@gmail.com'
-__version__ = '0.14.0a5'
+__version__ = '0.14.0a6'
 
 from advertools.ad_create import ad_create
 from advertools.ad_from_string import ad_from_string
