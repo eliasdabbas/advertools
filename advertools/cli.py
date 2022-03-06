84b5f252e0d86b682749f233ac5285c6376e9dde
Elias Dabbas <eliasdabbas@gmail.com>
2022-03-06 19:22:06 +0100

    Place _rev_dns function in cli module

--- advertools/cli.py
+++ advertools/cli.py
@@ -1,14 +1,20 @@
 import argparse
+import platform
+import socket
+from concurrent import futures
 from textwrap import dedent
 
 import pandas as pd
 
 import advertools as adv
-from _cli_rev_dns_lookup import _cli_reverse_dns_lookup
 from advertools import __version__
 
 pd.options.display.max_columns = None
 pd.options.display.width = 200
+
+system = platform.system()
+
+_default_max_workders = 60 if system == 'Darwin' else 600
 
 _entity_dict = {
     'emoji': adv.emoji.EMOJI,
@@ -36,6 +42,40 @@ def _format_df(df, head=10, precision=1):
         if df[col].dtype == float:
             df[col] = [format(n, f',.{precision}f') for n in df[col]]
     return df.head(head)
+
+
+def _single_request(ip):
+    try:
+        hostname, aliaslist, ipaddrlist = socket.gethostbyaddr(ip)
+        return [ip, hostname, '@@'.join(aliaslist), '@@'.join(ipaddrlist)]
+    except Exception as e:
+        return [ip, None, None, None, str(e)]
+
+
+def _cli_reverse_dns_lookup(ip_list, max_workers=600):
+    socket.setdefaulttimeout(8)
+    count_df = (pd.Series(ip_list)
+                .value_counts()
+                .reset_index())
+    count_df.columns = ['ip_address', 'count']
+    count_df['cum_count'] = count_df['count'].cumsum()
+    count_df['perc'] = count_df['count'].div(count_df['count'].sum())
+    count_df['cum_perc'] = count_df['perc'].cumsum()
+
+    hosts = []
+    with futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for host in executor.map(_single_request, count_df['ip_address']):
+            hosts.append(host)
+    df = pd.DataFrame(hosts)
+    columns = ['ip', 'hostname', 'aliaslist', 'ipaddrlist', 'errors']
+    if df.shape[1] == 4:
+        columns = columns[:-1]
+    df.columns = columns
+
+    final_df = pd.merge(count_df, df, left_on='ip_address',
+                        right_on='ip', how='left').drop('ip', axis=1)
+    return final_df
+
 
 
 class RawTextDefArgFormatter(argparse.RawTextHelpFormatter,
