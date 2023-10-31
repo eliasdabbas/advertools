1a38bf4546cd8d27cd10c9061e1dd576f301c427
Elias Dabbas <eliasdabbas@gmail.com>
2023-10-31 15:34:17 +0000

    Fix function typo

--- tests/test_header_spider.py
+++ tests/test_header_spider.py
@@ -12,5 +12,5 @@ def test_crawl_headers_raises_on_wrong_file_extension():
 
 @pytest.mark.parametrize("column", ["url", "crawl_time", "status"])
 def test_crawl_headers_returns_df(headers_crawl_df, column):
-    if platform.system != "Windows":
+    if platform.system() != "Windows":
         assert column in headers_crawl_df
