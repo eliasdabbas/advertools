9ed47982bdf617e2e4f8675554e17a68442611cd
Elias Dabbas <eliasdabbas@gmail.com>
2024-02-24 13:02:02 +0300

    sitemap_to_df: make test for optional resp_headers

--- tests/test_sitemaps.py
+++ tests/test_sitemaps.py
@@ -1,25 +1,32 @@
 import os
-from advertools.sitemaps import sitemap_to_df
+from pathlib import Path
+
 import pandas as pd
 import pytest
 
-gh_test_data_folder = 'https://raw.githubusercontent.com/eliasdabbas/advertools/master/tests/data/sitemap_testing/'
+from advertools.sitemaps import sitemap_to_df
+
+gh_test_data_folder = "https://raw.githubusercontent.com/eliasdabbas/advertools/master/tests/data/sitemap_testing/"
+offline_test_data_folder = "tests/data/sitemap_testing/"
 
 
 def full_path(file):
-    # tests_dir = f'/{folder}/'
-    # return 'file://' + os.path.abspath(tests_dir + file)
     return gh_test_data_folder + file
 
 
-regular_sitemap_url = full_path('regular_sitemap.xml')
-zipped_sitemap_url = full_path('zipped_sitemap.xml.gz')
-sitemap_index_url = full_path('sitemap_index.xml')
-error_sitemap_url = full_path('error_sitemap.xml')
-image_sitemap_url = full_path('image_sitemap.xml')
-video_sitemap_url = full_path('video_sitemap.xml')
-news_sitemap_url = full_path('news_sitemap.xml')
-robotstxt_url = full_path('robots.txt')
+def offline_path(filename):
+    path = Path(offline_test_data_folder + filename)
+    return path.absolute().as_uri()
+
+
+regular_sitemap_url = full_path("regular_sitemap.xml")
+zipped_sitemap_url = full_path("zipped_sitemap.xml.gz")
+sitemap_index_url = full_path("sitemap_index.xml")
+error_sitemap_url = full_path("error_sitemap.xml")
+image_sitemap_url = full_path("image_sitemap.xml")
+video_sitemap_url = full_path("video_sitemap.xml")
+news_sitemap_url = full_path("news_sitemap.xml")
+robotstxt_url = full_path("robots.txt")
 
 
 def test_regular_sitemap():
@@ -37,12 +44,10 @@ def test_gz_sitemap():
 def test_sitemap_index():
     result = sitemap_to_df(sitemap_index_url)
     assert isinstance(result, pd.core.frame.DataFrame)
-    assert 'errors' in result
-    errors = {'WARNING: Sitemap contains a link to itself',
-              'HTTP Error 404: Not Found'}
-    assert errors.issubset(result['errors'])
-    assert all([col in result
-               for col in ['loc', 'download_date', 'sitemap']])
+    assert "errors" in result
+    errors = {"WARNING: Sitemap contains a link to itself", "HTTP Error 404: Not Found"}
+    assert errors.issubset(result["errors"])
+    assert all([col in result for col in ["loc", "download_date", "sitemap"]])
 
 
 def test_error_sitemap():
@@ -53,21 +58,28 @@ def test_error_sitemap():
 def test_image_sitemap():
     result = sitemap_to_df(image_sitemap_url)
     assert isinstance(result, pd.core.frame.DataFrame)
-    assert 'image' in result
+    assert "image" in result
 
 
 def test_video_sitemap():
     result = sitemap_to_df(video_sitemap_url)
     assert isinstance(result, pd.core.frame.DataFrame)
-    assert 'video_content_loc' in result
+    assert "video_content_loc" in result
 
 
 def test_news_sitemap():
     result = sitemap_to_df(news_sitemap_url)
     assert isinstance(result, pd.core.frame.DataFrame)
-    assert 'news' in result
+    assert "news" in result
 
 
 def test_get_sitemaps_from_robotstxt():
     result = sitemap_to_df(robotstxt_url)
     assert isinstance(result, pd.core.frame.DataFrame)
+
+
+def test_sitemaps_offline():
+    sitemap_path = offline_path("regular_sitemap.xml")
+    result = sitemap_to_df(sitemap_path)
+    print(result.head())
+    assert isinstance(result, pd.DataFrame)
