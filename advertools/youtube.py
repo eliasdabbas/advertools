ef6a83a406f0ff323c1a711c357bbe31ef9b5721
Elias Dabbas <eliasdabbas@gmail.com>
2026-06-30 15:49:37 +0100

    Convert youtube.search docstring to raw string
    Completes PR #414: the search() docstring contains Freebase topic IDs with
    \_ sequences (e.g. /m/0jm\_) that still raised an invalid escape sequence
    SyntaxWarning. Prefix it with r as well.

--- advertools/youtube.py
+++ advertools/youtube.py
@@ -307,7 +307,7 @@ def search(
     videoSyndicated=None,
     videoType=None,
 ):
-    """Returns a collection of search results that match the query parameters
+    r"""Returns a collection of search results that match the query parameters
     specified in the API request. By default, a search result set identifies
     matching  video ,  channel , and  playlist  resources, but you can also
     configure queries to only retrieve a specific type of resource.
